a1cb9a8574d77cd4d1fa5bb8174a5ea9550c2ef3
Rob Messenger <robmessenger@Robs-MacBook-Air.local>
2018-06-19 17:52:56 +0100

    validate inputs with accessors on person proto

--- person_factory.js
+++ person_factory.js
@@ -1,14 +1,48 @@
 const person = Object.create({}, {
-  name: {},
-  birthDate: {},
-  deathDate: {}
+  name: {
+    get() {return name;},
+    set(newName) {
+      try {
+        if (typeof newName != 'string') throw 'name must be a string';
+      }
+      catch(error) {
+        return error;
+      }
+      name = newName;
+    }
+  },
+  birthDate: {
+    get() {return birthDate;},
+    set(newBirth) {
+      if (typeof newBirth != 'number') {
+        throw 'birth date must be a number';
+      }
+      birthDate = newBirth;
+    }
+  },
+  deathDate: {
+    get() {return deathDate;},
+    set(newDeath) {
+      if (typeof newDeath != 'number') {
+        throw 'death date must be a number';
+      }
+      if (typeof this.birthDate == undefined || newDeath < this.birthDate) {
+        throw 'birth date must preceed death date';
+      }
+      deathDate = newDeath;
+    }
+  }
 });
 
 function createPerson(name, birthDate, deathDate) {
-  newPerson = Object.create(person, {
-    name: {value: name},
-    birthDate: {value: birthDate},
-    deathDate: {value: deathDate}
-  });
+  newPerson = Object.create(person);
+  try {
+    newPerson.name = name;
+    newPerson.birthDate = birthDate;
+    newPerson.deathDate = deathDate;
+  }
+  catch(error) {
+    console.log('Invalid person: ' + error);
+  }
   return newPerson;
 }
