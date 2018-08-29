4a5d54eb16dab9aab2b8d49a8389b378b684865a
Rob <robmessenger@iMac.lan>
2018-08-29 16:11:01 +0200

    fix: prevent createPerson updating all persons

--- person_factory.js
+++ person_factory.js
@@ -1,53 +1,51 @@
-const person = Object.create({}/*, {
-  name: {
-    get() {return name;},
-    set(newName) {
-      try {
-        if (typeof newName != 'string') throw 'name must be a string';
-      }
-      catch(error) {
-        throw error;
-      }
-      name = newName;
+function descriptor (value) {
+  let descriptor = Object.create(null);
+  descriptor.value = value;
+  descriptor.enumerable = true;
+  return descriptor;
+}
+
+const person = {
+  setName: function (name) {
+    try {
+      if (typeof name != 'string') throw 'name must be a string';
     }
+    catch(error) {
+      throw error;
+    }
+    Object.defineProperty(this, 'name', descriptor(name));
   },
-  birthDate: {
-    get() {return birthDate;},
-    set(newBirth) {
-      try {
-        if (typeof newBirth != 'number') throw 'birth date must be a number';
-      }
-      catch(error) {
-        throw error;
-      }
-      birthDate = newBirth;
+  setBirthDate: function (birthDate) {
+    try {
+      if (typeof birthDate != 'number') throw 'birth date must be a number';
     }
+    catch(error) {
+      throw error;
+    }
+    Object.defineProperty(this, 'birthDate', descriptor(birthDate));
   },
-  deathDate: {
-    get() {return deathDate;},
-    set(newDeath) {
-      try {
-        if (typeof newDeath != 'number') throw 'death date must be a number';
-        if (typeof this.birthDate == undefined) throw 'birth date must be defined';
-        if (newDeath < this.birthDate) throw 'birth date must precede death date';
-      }
-      catch(error) {
-        throw error;
-      }
-      deathDate = newDeath;
+  setDeathDate: function (deathDate) {
+    try {
+      if (typeof deathDate != 'number') throw 'death date must be a number';
+      if (typeof this.birthDate == undefined) throw 'birth date must be defined';
+      if (deathDate < this.birthDate) throw 'birth date must precede death date';
     }
+    catch(error) {
+      throw error;
+    }
+    Object.defineProperty(this, 'deathDate', descriptor(deathDate));
   }
-}*/);
+};
 
 function createPerson(name, birthDate, deathDate) {
-  newPerson = Object.create(person);
+  let newPerson = Object.create(person);
   try {
-    newPerson.name = name;
-    newPerson.birthDate = birthDate;
-    newPerson.deathDate = deathDate;
-    return newPerson;
+    newPerson.setName(name);
+    newPerson.setBirthDate(birthDate);
+    newPerson.setDeathDate(deathDate);
   }
   catch(error) {
     console.log('Invalid person: ' + error);
   }
-}
+  return newPerson;
+};
