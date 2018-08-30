9fb54c75eb5bbffc14aae4019e0fe0d0b5b57c7f
Rob <robmessenger@iMac.lan>
2018-08-30 19:33:15 +0200

    feat: invoke form on a modal

--- addEpoch.js
+++ addEpoch.js
@@ -1,10 +1,22 @@
-$('form').submit((event) => {
+const toggleVisibility = function() {
+  let hidden = modal.is('.hide');
+  modal.toggleClass('hide', !hidden);
+  modal.toggleClass('show', hidden);
+}
+
+const modal = $('#modal')
+const form = $('form')
+
+$('button[name=add-epoch-invoke]').click(toggleVisibility)
+
+$(form).submit((event) => {
   let epochName = $('input[name=name]').val(),
       epochStart = Number($('input[name=start-year]').val()),
       epochEnd = Number($('input[name=end-year]').val());
   newEpoch = createEpoch(epochName, epochStart, epochEnd);
   epochs.addEpoch(newEpoch);
   epochs.updateEpochsView();
-  $('form')[0].reset();
+  $(form)[0].reset();
+  toggleVisibility(); 
   event.preventDefault();
 });
