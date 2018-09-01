2d90d91b46821536f49cae4a905a1df9eabe6683
Rob <robmessenger@iMac.lan>
2018-09-01 10:38:56 +0200

    feat: support open-ended epochs

--- addEpoch.js
+++ addEpoch.js
@@ -1,3 +1,8 @@
+function thisYear() {
+  today = new Date();
+  return today.getFullYear();
+}
+
 const toggleVisibility = function() {
   let hidden = modal.is('.hide');
   modal.toggleClass('hide', !hidden);
@@ -12,7 +17,7 @@ $('button[name=add-epoch-invoke]').click(toggleVisibility)
 $(form).submit((event) => {
   let epochName = $('input[name=name]').val(),
       epochStart = Number($('input[name=start-year]').val()),
-      epochEnd = Number($('input[name=end-year]').val());
+      epochEnd = Number($('input[name=end-year]').val() || thisYear());
   newEpoch = createEpoch(epochName, epochStart, epochEnd);
   epochs.addEpoch(newEpoch);
   epochs.updateEpochsView();
