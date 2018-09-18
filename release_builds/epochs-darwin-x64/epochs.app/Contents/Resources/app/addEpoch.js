84360aee85ac48ca6a555076b8f8c976c54f39ca
Rob <robmessenger@iMac.lan>
2018-09-18 12:41:19 +0200

    set up electron packager, get icon assets and package for mac

---
+++ release_builds/epochs-darwin-x64/epochs.app/Contents/Resources/app/addEpoch.js
@@ -0,0 +1,31 @@
+window.$ = window.jQuery = require('jquery'); //TODO: why did this fix it? 
+
+function thisYear() {
+  today = new Date();
+  return today.getFullYear();
+}
+
+const toggleVisibility = function() {
+  let hidden = modal.is('.hide');
+  modal.toggleClass('hide', !hidden);
+  modal.toggleClass('show', hidden);
+}
+
+const modal = $('#modal')
+const form = $('form')
+
+$('button[name=add-epoch-invoke]').click(toggleVisibility)
+
+$(form).submit((event) => {
+  let epochName = $('input[name=name]').val(),
+      epochStart = Number($('input[name=start-year]').val()),
+      epochEnd = Number($('input[name=end-year]').val() || thisYear());
+  newEpoch = createEpoch(epochName, epochStart, epochEnd);
+  epochs.addEpoch(newEpoch);
+  epochs.updateEpochsView();
+  $(form)[0].reset();
+  toggleVisibility();
+  event.preventDefault();
+});
+
+$('button[name=cancel-add-epoch]').click(toggleVisibility)
